c5569fadf4c0a90343af3329fe602c5a79112265
ursinasanderink <sanderink.ursina@yahoo.com>
2021-12-12 11:51:50 -0600

    ,

--- app/src/main/java/com/example/thehungrydeveloper/MainActivity.java
+++ app/src/main/java/com/example/thehungrydeveloper/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.thehungrydeveloper;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
+import android.nfc.cardemulation.CardEmulation;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    CardView startersCard;
+    CardView mainsCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startersCard = findViewById(R.id.card_view_starters);
+        mainsCard = findViewById(R.id.card_view_mains);
+
+
+        startersCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent startersActivityIntent = new Intent(MainActivity.this, StarterActivity.class);
+
+                startActivity(startersActivityIntent);
+            }
+        });
     }
 }
